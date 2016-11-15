02ee7647bdac1e4fa1ad2fef281a8aeaeaf0d398
Benjamin White <benjamin.e.white1@gmail.com>
2016-11-15 15:16:50 -0500

    updates

---
+++ server.js
@@ -0,0 +1,108 @@
+
+const express = require('express');
+const router = express.Router();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+
+const {ShoppingList, Recipes} = require('./models');
+
+const jsonParser = bodyParser.json();
+const app = express();
+
+// log the http layer
+app.use(morgan('common'));
+
+// we're going to add some items to ShoppingList
+// so there's some data to look at
+ShoppingList.create('beans', true);
+ShoppingList.create('tomatoes', false);
+ShoppingList.create('peppers', false);
+
+// adding some recipes to `Recipes` so there's something
+// to retrieve.
+Recipes.create(
+  'boiled white rice', ['1 cup white rice', '2 cups water', 'pinch of salt']);
+Recipes.create(
+  'milkshake', ['2 tbsp cocoa', '2 cups vanilla ice cream', '1 cup milk']);
+
+// when the root of this router is called with GET, return
+// all current ShoppingList items
+app.get('/shopping-list', (req, res) => {
+  res.json(ShoppingList.get());
+});
+
+app.post('/shopping-list', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['name', 'budget'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+
+  const item = ShoppingList.create(
+    {name: req.body.name, budget: req.body.budget});
+  res.status(201).json(item);
+});
+
+// when PUT request comes in with updated item, ensure has
+// required fields. also ensure that item id in url path, and
+// item id in updated item object match. if problems with any
+// of that, log error and send back status code 400. otherwise
+// call `ShoppingList.update` with updated item.
+app.put('/shopping-list/:id', jsonParser, (req, res) => {
+  const requiredFields = ['name', 'checked', 'id'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  if (req.params.id !== req.body.id) {
+    const message = (
+      `Request path id (${req.params.id}) and request body id `
+      `(${req.body.id}) must match`);
+    console.error(message);
+    return res.status(400).send(message);
+  }
+  console.log(`Updating shopping list item \`${req.params.id}\``);
+  const updatedItem = ShoppingList.update({
+    id: req.params.id,
+    name: req.body.name,
+    checked: req.body.checked
+  });
+  res.status(204).json(updatedItem);
+});
+
+
+// when new recipe added, ensure has required fields. if not,
+// log error and return 400 status code with hepful message.
+// if okay, add new item, and return it with a status 201.
+app.post('/recipes', jsonParser, (req, res) => {
+  // ensure `name` and `budget` are in request body
+  const requiredFields = ['name', 'ingredients'];
+  for (let i=0; i<requiredFields.length; i++) {
+    const field = requiredFields[i];
+    if (!(field in req.body)) {
+      const message = `Missing \`${field}\` in request body`
+      console.error(message);
+      return res.status(400).send(message);
+    }
+  }
+  const item = Recipes.create(req.body.name, req.body.ingredients);
+  res.status(201).json(item);
+});
+
+
+app.get('/recipes', (req, res) => {
+  res.json(Recipes.get());
+})
+
+app.listen(process.env.PORT || 8080, () => {
+  console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
+});
