bf175742dd786d0631f9fca7903ce432cef15ef3
Justin Koeppen <jdkoeppen@gmail.com>
2018-03-07 14:07:30 -0800

    Added DELETE method to Recipes

--- server.js
+++ server.js
@@ -77,6 +77,11 @@ app.get('/recipes', (req, res) => {
   res.json(Recipes.get());
 })
 
+app.delete('/recipes/:id', (req, res) => {
+  Recipes.delete(req.params.id);
+  console.log(`Deleted shopping list item \`${req.params.id}\``);
+  res.status(204).end();
+})
 app.listen(process.env.PORT || 8080, () => {
   console.log(`Your app is listening on port ${process.env.PORT || 8080}`);
 });
